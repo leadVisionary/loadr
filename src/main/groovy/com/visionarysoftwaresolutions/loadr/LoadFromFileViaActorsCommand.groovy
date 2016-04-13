41dee20ca5137698122a026fccfcdbe320d21dd5
V <nvaidyanathan@whitepages.com>
2016-04-13 15:13:07 -0700

    make Loader use a Command too.

---
+++ src/main/groovy/com/visionarysoftwaresolutions/loadr/LoadFromFileViaActorsCommand.groovy
@@ -0,0 +1,35 @@
+package com.visionarysoftwaresolutions.loadr
+
+import com.visionarysoftwaresolutions.loadr.api.Command
+import groovyx.gpars.actor.StaticDispatchActor
+
+import java.util.function.Supplier
+
+final class LoadFromFileViaActorsCommand implements Command<File> {
+    private final Supplier<StaticDispatchActor<File>> supplier
+
+    LoadFromFileViaActorsCommand(final Supplier<StaticDispatchActor<File>> supplier) {
+        if (supplier == null) {
+            throw new IllegalArgumentException("should not get null supplier")
+        }
+        this.supplier = supplier
+    }
+
+
+    @Override
+    void execute(final File parameter) {
+        if (parameter == null || !parameter.isFile()) {
+            throw new IllegalArgumentException("Should get valid file")
+        }
+        final StaticDispatchActor<File> reader = supplier.get()
+        if( reader == null ) {
+            throw new IllegalStateException("Supplier returned null")
+        }
+
+        if(!reader.active) {
+            reader.start()
+        }
+        reader << parameter
+        reader.join()
+    }
+}
--- src/main/groovy/com/visionarysoftwaresolutions/loadr/Loader.groovy
+++ src/main/groovy/com/visionarysoftwaresolutions/loadr/Loader.groovy
@@ -1,11 +1,9 @@
 package com.visionarysoftwaresolutions.loadr
 
 import com.visionarysoftwaresolutions.loadr.actors.CommandBasedActorSupplier
-import groovyx.gpars.actor.StaticDispatchActor
 import org.slf4j.Logger
 
 import java.util.function.Function
-import java.util.function.Supplier
 
 public final class Loader {
     public static <T, U> void loadFromFileWithDelimiterAndPublishers(final File source,
@@ -13,24 +11,6 @@ public final class Loader {
                                                                   final Logger log,
                                                                   final Function<T, U> mapper,
                                                                   final Function<String, T> stringTransform) {
-        final Supplier<StaticDispatchActor<File>> c =
-                new CommandBasedActorSupplier(publishers, log, mapper, stringTransform)
-        new Loader(c.get(), source).load()
-    }
-
-    private final StaticDispatchActor<File> reader
-    private final File source
-
-    private Loader(final StaticDispatchActor<File> reader, final File source) {
-        this.reader = reader
-        this.source = source
-    }
-
-    private void load() {
-        if(!reader.active) {
-            reader.start()
-        }
-        reader << source
-        reader.join()
+        new LoadFromFileViaActorsCommand(new CommandBasedActorSupplier(publishers, log, mapper, stringTransform)).execute(source)
     }
 }
