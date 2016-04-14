07f17edefb80f285abcf6b341ea6ace0adc756ca
V <nvaidyanathan@whitepages.com>
2016-04-14 01:00:19 -0700

    add tests for Supplier.

--- src/main/groovy/com/visionarysoftwaresolutions/loadr/actors/store/BlackboardSupplier.groovy
+++ src/main/groovy/com/visionarysoftwaresolutions/loadr/actors/store/BlackboardSupplier.groovy
@@ -1,13 +1,18 @@
 package com.visionarysoftwaresolutions.loadr.actors.store
 
 import com.visionarysoftwaresolutions.loadr.api.CloseableRepository
-import groovy.transform.Immutable
 import groovyx.gpars.actor.StaticDispatchActor
 import java.util.function.Supplier
 
-@Immutable
 final class BlackboardSupplier<T> implements Supplier<CloseableRepository<T>> {
     private final Supplier<StaticDispatchActor<T>> supplier
+
+    BlackboardSupplier(final Supplier<StaticDispatchActor<T>> supplier) {
+        if (supplier == null) {
+            throw new IllegalArgumentException("should not get null supplier")
+        }
+        this.supplier = supplier
+    }
 
     @Override
     CloseableRepository<T> get() {
